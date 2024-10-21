13b9669332bccad9bc1607bb45119788d1043ebd
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-10-21 13:00:26 +0200

    add mau django app fixes #3

---
+++ mau_app/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from .models import GreetingsModel
+
+admin.site.register(GreetingsModel)
---
+++ mau_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MauAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mau_app'
