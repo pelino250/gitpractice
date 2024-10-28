ea31946071f4a65bc5e98deb31e65d1a7ba65abf
Vulgence <e.kinyua@alustudent.com>
2024-10-28 21:48:11 +0300

    models

--- elvis/models.py
+++ elvis/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Bike(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(default='default-slug')
+    description = models.TextField()
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity_in_stock = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='bike_images', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        permissions = [
+            ("can_manage_bike", "Can manage bike"),
+        ]
+
+    def __str__(self):
+        return self.name
--- elvis/views.py
+++ elvis/views.py
@@ -1,4 +1,11 @@
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Bike
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
 
-def hello_elvis(request):
-    return HttpResponse("Hello there!")
+class BikeListView(generics.ListAPIView):
+    queryset = Bike.objects.all()
+    permission_classes = [IsAuthenticated]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter]
+    search_fields = ['name', 'description']
